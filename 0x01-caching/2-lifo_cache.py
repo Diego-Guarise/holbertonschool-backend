084e7031fcc8a3a7c481f2672f7830fbd9c7c62e
Diego-Guarise <l.guariselopez@gmail.com>
2022-05-13 19:31:58 -0300

    all

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,29 +1,40 @@
-
 #!/usr/bin/python3
-""" 2-main LIFO """
+"""
+LIFO Caching
+"""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ LIFOCache inherit from BaseCaching """
-    list_aux = []
+    """
+    Create a class LIFOCache that inherits
+    from BaseCaching and is a caching system:
+    """
+    list = []
 
     def __init__(self):
         super().__init__()
 
     def put(self, key, item):
-        """ Add LIFO """
+        """
+        Must assign to the dictionary self.cache_data
+        the item value for the key key.
+        """
         if key is None or item is None:
             return
-        self.list_aux.append(key)
+        self.list.append(key)
         self.cache_data[key] = item
         if len(self.cache_data) > self.MAX_ITEMS:
-            del self.cache_data[self.list_aux[len(self.list_aux) - 2]]
-            print("DISCARD: {}".format(self.list_aux[len(self.list_aux) - 2]))
+            del self.cache_data[self.list[len(self.list) - 2]]
+            print("DISCARD: {}".format(self.list[len(self.list) - 2]))
 
     def get(self, key):
-        """ Get FIFO """
+        """
+        Must return the value in self.cache_data linked
+        to key.
+        """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
+    
