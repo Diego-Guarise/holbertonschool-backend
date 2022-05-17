a3a0aa4e047425a170149b3a6b6b57ccadbd0254
Diego-Guarise <l.guariselopez@gmail.com>
2022-05-17 19:45:57 -0300

    pycode y fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,10 +9,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
     """display index page"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
