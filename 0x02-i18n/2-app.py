835ecee96b609e0c539cc167bee9e58aecddf8fe
Diego-Guarise <l.guariselopez@gmail.com>
2022-05-17 20:27:12 -0300

    task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -15,7 +15,7 @@ babel = Babel(app)
 @app.route('/', methods=['GET', 'POST'], strict_slashes=False)
 def index():
     """display index page"""
-    return render_template('2-index.html')
+    return render_template('1-index.html')
 
 
 class Config(object):
@@ -28,11 +28,12 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config.from_object('2-app.Config')
+app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
+    """ get locale from request """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
