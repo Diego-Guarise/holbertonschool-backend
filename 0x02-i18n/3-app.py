d1e684f3b922b4c849f45d1de35cf8fd732f4387
Diego-Guarise <l.guariselopez@gmail.com>
2022-05-17 20:53:57 -0300

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@app.route('/', methods=['GET', 'POST'], strict_slashes=False)
+def index():
+    """display index page"""
+    return render_template('3-index.html')
+
+
+class Config(object):
+    """
+    Basic Flask app
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ get locale from request """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
