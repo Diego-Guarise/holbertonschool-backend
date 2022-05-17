64ff773abc453fb044c9441583f6fcf5b9b18f55
Diego-Guarise <l.guariselopez@gmail.com>
2022-05-17 20:05:27 -0300

    import babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,9 +5,11 @@ Basic Flask app
 
 
 from flask import Flask, render_template
+from flask_babel import Babel
 
 
 app = Flask(__name__)
+babel = Babel(app)
 
 
 @app.route('/', methods=['GET', 'POST'], strict_slashes=False)
