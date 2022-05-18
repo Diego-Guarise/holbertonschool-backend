d5ef23113ca3c9701df0163aa012765630978c4d
Diego-Guarise <l.guariselopez@gmail.com>
2022-05-17 23:59:46 -0300

    a

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -63,7 +63,7 @@ def get_locale():
 @app.route('/', methods=['GET', 'POST'], strict_slashes=False)
 def index():
     """display index page"""
-    return render_template('5-index.html')
+    return render_template('6-index.html')
 
 
 if __name__ == '__main__':
